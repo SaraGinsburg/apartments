c480771bf3de229cdef1533b41d6fef716bfb928
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2018-05-07 09:49:34 -0400

    initial commit

---
+++ lib/apartments.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+
+require_relative "./apartments/version"
+require_relative "./apartments/deal"
+require_relative "./apartments/cli"
+
+# module Apartments
+#   # Your code goes here...
+# end
